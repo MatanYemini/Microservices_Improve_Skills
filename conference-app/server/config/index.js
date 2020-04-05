a85c2dfdc83558b1297334b9cde01b6e33a9ba6a
Yemini <yeminimatan@gmail.com>
2020-04-05 17:47:49 +0300

    Adding Confrence, Feedback, Registry, Speakers

---
+++ conference-app/server/config/index.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  development: {
+    sitename: 'Roux Meetups [Development]',
+    serviceRegistryUrl: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
+  },
+  production: {
+    sitename: 'Roux Meetups',
+    serviceRegistryUrl: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
+  },
+};
